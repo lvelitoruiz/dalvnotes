4e5394a8620720b9090f8b2c642d0f5516c2fd65
lvelitoruizorion <luis.velito@orion.global>
2025-05-01 20:49:44 -0500

    updated

--- src/components/Post/PostContent.tsx
+++ src/components/Post/PostContent.tsx
@@ -22,6 +22,7 @@ export const PostContent = ({ content }: PostContentProps) => {
       wrapper.appendChild(table);
     }
   }, [content]);
+  
 
   return (
     <div className="container mx-auto px-4 py-10 max-w-[960px]">
